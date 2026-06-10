0f03cb904ca09635adcedaee6ea64c546be00183
Thomaz <122468659+Th0mzzz@users.noreply.github.com>
2026-06-09 23:36:00 -0300

    first commit

---
+++ src/main/java/com/mentoai/mentoaiapi/MentoaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.mentoai.mentoaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoaiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoaiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mentoai/mentoaiapi/MentoaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoai.mentoaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoaiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
